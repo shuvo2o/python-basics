08d19ee3ea3aab2bf233e7ae45431b7dca24246e
shuvo2o <eng.shuvo03@gmail.com>
2025-12-01 09:23:02 +0600

    basic python practice

---
+++ practice1.py
@@ -0,0 +1,3 @@
+num1 =input("Enter first number: ")
+num2 =input("Enter second number: ")
+print("Sum is: ", int(num1)+ int(num2))
---
+++ variables.py
@@ -0,0 +1,7 @@
+name = "Shuvo",
+age = 25,
+is_student = True,
+marks=[85, 90, 78],
+address={"city": "Dhaka", "country": "Bangladesh"}
+print(address["city"])
+print(name)
